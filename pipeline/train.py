e31b62eb538f51f929491c98949ced2e22eeec77
Jason Thomo <jthomo@uvic.ca>
2025-01-27 12:00:04 -0800

    boost hyperparam grid, fix SimpleDNN pipeline

--- pipeline/train.py
+++ pipeline/train.py
@@ -1,16 +1,18 @@
 import sys
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
+from ray import tune
 import warnings
 from process import data_prep_pipeline
 from SimpleDNN import (
     SimpleDNN,
     SimpleLinear,
-    train_nn_model,
+    train_simple_dnn,
     hyperparam_search_simple_dnn,
 )
 from Scaden import ScadenModel, train_scaden_ensemble, predict_ensemble
@@ -28,61 +30,65 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def pipeline_simple_dnn():
-    X_train, X_test, Y_train, Y_test = data_prep_pipeline(
+    # B is model input, C is model output
+    B, C = data_prep_pipeline(
         BULK_PATH, SC_PATH, SC_METADATA_PATH, n_aug=30, aug_ratio=0.9
     )
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    train_dataset = TensorDataset(
-        torch.tensor(X_train, dtype=torch.float32),
-        torch.tensor(Y_train, dtype=torch.float32),
-    )
-    test_dataset = TensorDataset(
-        torch.tensor(X_test, dtype=torch.float32),
-        torch.tensor(Y_test, dtype=torch.float32),
-    )
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
     # Hyperparam search for finding best SimpleDNN architecture
     param_grid = {
-        "input_dim": [X_train.shape[1]],
-        "output_dim": [Y_train.shape[1]],
-        "lr": [0.001],
-        "epochs": [150],
-        "criterion": [nn.MSELoss()],  # TODO: experiment w/ training loss functions
-        "dropout_rate": [0.05, 0.1, 0.2],
+        "batch_size": [32, 64, 128],
+        "lr": [0.001, 0.0001],
+        "epochs": [1000],
+        "patience": [30],
+        "criterion": [nn.L1Loss(), nn.MSELoss()],
         "hidden_layers": [
-            [32],
-            [64],
-            [128],
-            [32, 32],
-            [64, 32],
-            [128, 32],
-            [128, 32, 64],
+            ([32], [0.1]),
+            ([32, 16], [0.2, 0.1]),
+            ([32, 32], [0.2, 0.1]),
+            ([32, 32, 16], [0.2, 0.2, 0.1]),
+            ([64], [0.1]),
+            ([64, 16], [0.2, 0.1]),
+            ([64, 32], [0.2, 0.1]),
+            ([64, 32, 16], [0.2, 0.2, 0.1]),
+            ([96], [0.1]),
+            ([96, 32], [0.2, 0.1]),
+            ([96, 64], [0.2, 0.1]),
+            ([96, 64, 32], [0.2, 0.2, 0.1]),
+            ([128], [0.2]),
+            ([128, 32], [0.2, 0.1]),
+            ([128, 64], [0.2, 0.1]),
+            ([128, 64, 32], [0.2, 0.2, 0.1]),
+            ([256], [0.2]),
+            ([256, 32], [0.2, 0.1]),
+            ([256, 128], [0.2, 0.1]),
+            ([256, 128, 64], [0.2, 0.2, 0.1]),
         ],
     }
-    best_params = hyperparam_search_simple_dnn(
-        SimpleDNN, param_grid, X_train, Y_train, k=3
-    )
-    model_params = {
-        k: v for k, v in best_params.items() if k not in ["lr", "epochs", "criterion"]
-    }
-    model = SimpleDNN(**model_params).to(DEVICE)
+
+    best_params = hyperparam_search_simple_dnn(B, C, param_grid, k=3)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(B, C, test_size=0.2)
+
+    final_model = SimpleDNN(
+        best_params["input_dim"],
+        best_params["output_dim"],
+        best_params["hidden_layers"],
+    ).to(DEVICE)
 
     print("\nTraining final model with best parameters...")
-    train_nn_model(
-        model,
-        train_loader,
-        test_loader,
-        optimizer=optim.Adam(model.parameters(), best_params["lr"]),
+    train_simple_dnn(
+        final_model,
+        X_train,
+        X_test,
+        Y_train,
+        Y_test,
         criterion=best_params["criterion"],
-        epochs=150,
+        batch_size=best_params["batch_size"],
+        lr=best_params["lr"],
     )
-    save_to_disk(model, X_test, Y_test, "SimpleDNN")
-    print_eval(X_test, Y_test, model=model, name="SimpleDNN")
+    save_to_disk(final_model, X_test, Y_test, "SimpleDNN")
+    print_eval(X_test, Y_test, model=final_model, name="SimpleDNN")
 
 
 def pipeline_simple_linear():
@@ -109,7 +115,7 @@ def pipeline_simple_linear():
     model = SimpleLinear(input_dim, output_dim)
 
     print("Training linear model...")
-    train_nn_model(
+    train_simple_dnn(
         model,
         train_loader,
         test_loader,
