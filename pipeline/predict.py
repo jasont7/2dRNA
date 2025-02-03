e596eed12af1d1ba6b26830a6b2a17cba26cfe9a
Jason Thomo <jthomo@uvic.ca>
2025-02-03 12:56:34 -0800

    use K-Fold CV for eval

--- pipeline/predict.py
+++ pipeline/predict.py
@@ -1,13 +1,13 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import torch
 import warnings
 from process import data_prep_pipeline
 from SimpleDNN import (
     SimpleDNN,
 )
-from utils import print_eval
+from utils import eval_metrics
 
 warnings.filterwarnings("ignore")
 np.set_printoptions(linewidth=120)
@@ -33,26 +33,47 @@ def load_weights(model: SimpleDNN, weights_path):
     model.to(DEVICE)
 
 
-def predict_simple_dnn(model_path, hidden_layers):
+def predict_simple_dnn(model_path, hidden_layers, n_splits=5):
     B, C = data_prep_pipeline(
         BULK_PATH, SC_PATH, SC_METADATA_PATH, n_aug=30, aug_ratio=0.9
     )
-
-    _, X_test, _, Y_test = train_test_split(B, C, test_size=0.2)
-    input_dim = X_test.shape[1]
-    output_dim = Y_test.shape[1]
-
     scaler = StandardScaler()
-    X_test = scaler.fit_transform(X_test)
+    B = scaler.fit_transform(B)
 
-    model = SimpleDNN(input_dim, output_dim, hidden_layers)
-    load_weights(model, model_path)
+    kfold = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    eval_results = []
 
-    print_eval(Y_test, model=model, X_test=X_test, name="SimpleDNN")
+    for train_idx, test_idx in kfold.split(B):
+        _, X_test = B[train_idx], B[test_idx]
+        _, Y_test = C[train_idx], C[test_idx]
+
+        model = SimpleDNN(
+            input_dim=B.shape[1], output_dim=C.shape[1], hidden_layers=hidden_layers
+        )
+        load_weights(model, model_path)
+
+        eval_result = eval_metrics(Y_test, model=model, X_test=X_test)
+        eval_results.append(eval_result)
+
+    avg_eval_result = {
+        "target_mean": np.mean([result["target_mean"] for result in eval_results]),
+        "target_var": np.mean([result["target_var"] for result in eval_results]),
+        "mse": np.mean([result["mse"] for result in eval_results]),
+        "r2": np.mean([result["r2"] for result in eval_results]),
+        "kl_div": np.mean([result["kl_div"] for result in eval_results]),
+        "wasserstein": np.mean([result["wasserstein"] for result in eval_results]),
+    }
+
+    print(f"\nAverage Evaluation Metrics Across K={n_splits} Folds:")
+    print(f" - Target value mean: {avg_eval_result['target_mean']:.7f}")
+    print(f" - Target value variance: {avg_eval_result['target_var']:.7f}")
+    print(f" - MSE: {avg_eval_result['mse']:.7f}")
+    print(f" - R²: {avg_eval_result['r2']:.7f}")
+    print(f" - KL Divergence: {avg_eval_result['kl_div']:.7f}")
+    print(f" - Wasserstein Distance: {avg_eval_result['wasserstein']:.7f}")
 
 
 def predict_ensemble_avg(models, X_test):
-    print("\nEnsemble predictions (average of 3 DNNs)...")
     predictions = np.zeros((X_test.shape[0], 11))
     for _, model in models:
         model.eval()
@@ -63,44 +84,73 @@ def predict_ensemble_avg(models, X_test):
     return predictions
 
 
-def predict_scaden(m256_path, m512_path, m1024_path):
+def predict_scaden(m256_path, m512_path, m1024_path, n_splits=5):
     B, C = data_prep_pipeline(
         BULK_PATH, SC_PATH, SC_METADATA_PATH, n_aug=30, aug_ratio=0.9
     )
-
-    _, X_test, _, Y_test = train_test_split(B, C, test_size=0.2)
-    input_dim = X_test.shape[1]
-    output_dim = Y_test.shape[1]
-
     scaler = StandardScaler()
-    X_test = scaler.fit_transform(X_test)
+    B = scaler.fit_transform(B)
 
-    m256 = SimpleDNN(input_dim, output_dim, ([256, 128, 64, 32], [0, 0, 0, 0]))
-    m512 = SimpleDNN(input_dim, output_dim, ([512, 256, 128, 64], [0, 0.3, 0.2, 0.1]))
-    m1024 = SimpleDNN(
-        input_dim, output_dim, ([1024, 512, 256, 128], [0, 0.6, 0.3, 0.1])
-    )
-    load_weights(m256, m256_path)
-    load_weights(m512, m512_path)
-    load_weights(m1024, m1024_path)
+    kfold = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    eval_results = []
 
-    Y_pred = predict_ensemble_avg(
-        [("m256", m256), ("m512", m512), ("m1024", m1024)], X_test
-    )
+    for train_idx, test_idx in kfold.split(B):
+        _, X_test = B[train_idx], B[test_idx]
+        _, Y_test = C[train_idx], C[test_idx]
 
-    print_eval(Y_test, Y_pred, name="ScadenEnsemble")
+        m256 = SimpleDNN(
+            input_dim=B.shape[1],
+            output_dim=C.shape[1],
+            hidden_layers=([256, 128, 64, 32], [0, 0, 0, 0]),
+        )
+        m512 = SimpleDNN(
+            input_dim=B.shape[1],
+            output_dim=C.shape[1],
+            hidden_layers=([512, 256, 128, 64], [0, 0.3, 0.2, 0.1]),
+        )
+        m1024 = SimpleDNN(
+            input_dim=B.shape[1],
+            output_dim=C.shape[1],
+            hidden_layers=([1024, 512, 256, 128], [0, 0.6, 0.3, 0.1]),
+        )
+        load_weights(m256, m256_path)
+        load_weights(m512, m512_path)
+        load_weights(m1024, m1024_path)
+
+        Y_pred = predict_ensemble_avg(
+            [("m256", m256), ("m512", m512), ("m1024", m1024)], X_test
+        )
+        eval_result = eval_metrics(Y_test, Y_pred)
+        eval_results.append(eval_result)
+
+    avg_eval_result = {
+        "target_mean": np.mean([result["target_mean"] for result in eval_results]),
+        "target_var": np.mean([result["target_var"] for result in eval_results]),
+        "mse": np.mean([result["mse"] for result in eval_results]),
+        "r2": np.mean([result["r2"] for result in eval_results]),
+        "kl_div": np.mean([result["kl_div"] for result in eval_results]),
+        "wasserstein": np.mean([result["wasserstein"] for result in eval_results]),
+    }
+
+    print(f"\nAverage Evaluation Metrics Across K={n_splits} Folds:")
+    print(f" - Target value mean: {avg_eval_result['target_mean']:.7f}")
+    print(f" - Target value variance: {avg_eval_result['target_var']:.7f}")
+    print(f" - MSE: {avg_eval_result['mse']:.7f}")
+    print(f" - R²: {avg_eval_result['r2']:.7f}")
+    print(f" - KL Divergence: {avg_eval_result['kl_div']:.7f}")
+    print(f" - Wasserstein Distance: {avg_eval_result['wasserstein']:.7f}")
 
 
 def main():
-    predict_simple_dnn(
-        model_path="output/2dRNA/20250128_0224/SimpleDNN.pth",
-        hidden_layers=([1024, 512], [0.05, 0.05]),
-    )
-    # predict_scaden(
-    #     m256_path="output/2dRNA/20250127_1614/m256.pth",
-    #     m512_path="output/2dRNA/20250127_1619/m512.pth",
-    #     m1024_path="output/2dRNA/20250127_1620/m1024.pth",
+    # predict_simple_dnn(
+    #     model_path="output/2dRNA/20250128_0224/SimpleDNN.pth",
+    #     hidden_layers=([1024, 512], [0.05, 0.05]),
     # )
+    predict_scaden(
+        m256_path="output/2dRNA/20250127_1614/m256.pth",
+        m512_path="output/2dRNA/20250127_1619/m512.pth",
+        m1024_path="output/2dRNA/20250127_1620/m1024.pth",
+    )
 
 
 if __name__ == "__main__":
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -36,61 +36,7 @@ def save_to_disk(model, X_test=None, Y_test=None, name="Model"):
         print(f"Saved true fractions to {true_fractions_file}")
 
 
-# def print_eval(Y_test, Y_pred=None, model=None, X_test=None, name="? Model"):
-#     """
-#     Print evaluation metrics for model on test set by comparing Y_test to Y_pred.
-#     Y_pred is the model's predictions on X_test IF a model is provided. Otherwise,
-#     Y_pred must be provided to be compared directly.
-#     """
-#     if model is None and Y_pred is None:
-#         raise ValueError("Either model or Y_pred must be provided.")
-
-#     if not isinstance(Y_test, torch.Tensor):
-#         Y_test = torch.tensor(Y_test, dtype=torch.float32)
-
-#     if model is not None:
-#         print("\Running model on X_test...")
-#         if not isinstance(X_test, torch.Tensor):
-#             X_test = torch.tensor(X_test, dtype=torch.float32)
-#         model.eval()
-#         with torch.no_grad():
-#             Y_pred = model(X_test.to(DEVICE)).cpu()
-
-#     if not isinstance(Y_pred, torch.Tensor):
-#         Y_pred = torch.tensor(Y_pred, dtype=torch.float32)
-
-#     target_min = Y_test.min().item()
-#     target_max = Y_test.max().item()
-#     target_mean = Y_test.mean().item()
-#     target_median = torch.median(Y_test).item()
-
-#     mae = torch.mean(torch.abs(Y_pred - Y_test)).item()
-#     rmse = torch.sqrt(torch.mean((Y_pred - Y_test) ** 2)).item()
-#     cosine = torch.nn.functional.cosine_similarity(Y_pred, Y_test, dim=1).mean().item()
-
-#     mae_pct_range = (mae / (target_max - target_min)) * 100
-#     mae_pct_mean = (mae / target_mean) * 100
-#     mae_pct_median = (mae / target_median) * 100
-#     rmse_pct_range = (rmse / (target_max - target_min)) * 100
-#     rmse_pct_mean = (rmse / target_mean) * 100
-#     rmse_pct_median = (rmse / target_median) * 100
-
-#     print(f"\nEvaluation Metrics for {name}:")
-#     print(f" - Target value range: [{target_min:.7f}, {target_max:.7f}]")
-#     print(f" - Target value mean: {target_mean:.7f}")
-#     print(f" - Target value median: {target_median:.7f}")
-#     print(f" - MAE: {mae:.7f}")
-#     print(f" - MAE as % of range: {mae_pct_range:.2f}%")
-#     print(f" - MAE as % of mean: {mae_pct_mean:.2f}%")
-#     print(f" - MAE as % of median: {mae_pct_median:.2f}%")
-#     print(f" - RMSE: {rmse:.7f}")
-#     print(f" - RMSE as % of range: {rmse_pct_range:.2f}%")
-#     print(f" - RMSE as % of mean: {rmse_pct_mean:.2f}%")
-#     print(f" - RMSE as % of median: {rmse_pct_median:.2f}%")
-#     print(f" - Cosine similarity: {cosine:.7f}")
-
-
-def print_eval(Y_test, Y_pred=None, model=None, X_test=None, name="? Model"):
+def eval_metrics(Y_test, Y_pred=None, model=None, X_test=None):
     """
     Print evaluation metrics for model on test set by comparing Y_test to Y_pred.
     Computes MSE, R², KL Divergence, and Wasserstein Loss.
@@ -104,7 +50,6 @@ def print_eval(Y_test, Y_pred=None, model=None, X_test=None, name="? Model"):
         Y_test = torch.tensor(Y_test, dtype=torch.float32)
 
     if model is not None:
-        print("\nRunning model on X_test...")
         if not isinstance(X_test, torch.Tensor):
             X_test = torch.tensor(X_test, dtype=torch.float32)
         model.eval()
@@ -137,10 +82,11 @@ def print_eval(Y_test, Y_pred=None, model=None, X_test=None, name="? Model"):
         / Y_test.shape[0]
     )
 
-    print(f"\nEvaluation Metrics for {name}:")
-    print(f" - Target value mean: {target_mean:.7f}")
-    print(f" - Target value variance: {target_var:.7f}")
-    print(f" - MSE: {mse:.7f}")
-    print(f" - R²: {r2:.7f}")
-    print(f" - KL Divergence: {kl_div:.7f}")
-    print(f" - Wasserstein Distance: {wasserstein:.7f}")
+    return {
+        "target_mean": target_mean,
+        "target_var": target_var,
+        "mse": mse,
+        "r2": r2,
+        "kl_div": kl_div,
+        "wasserstein": wasserstein,
+    }
